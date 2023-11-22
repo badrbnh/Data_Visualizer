5e8c95a36cdd192a3d9067a06d8192a961f1e437
badrbnh <badrechahide@gmail.com>
2023-11-22 03:38:44 +0100

    added login

--- models/user.py
+++ models/user.py
@@ -1,25 +1,36 @@
 from sqlalchemy import Column, String
 from sqlalchemy.orm import DeclarativeBase
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin
+from web_flask import login
 import uuid
 
 class Base(DeclarativeBase):
     pass
 
-class User(Base):
+class User(Base, UserMixin):
     """user class"""
     __tablename__ = 'users'
-    id = Column(String(36), primary_key=True, default=str(uuid.uuid4()))
+    id = Column(String(36), primary_key=True, default=lambda: str(uuid.uuid4()))
     username = Column(String(128), nullable=False)
-    password = Column(String(128), nullable=False)
+    password_hash = Column(String(256), nullable=False)
     email = Column(String(128), nullable=False)
 
     def __init__(self, username, password, email):
         """initializes user"""
-        self.id = str(uuid.uuid4())  # Ensure each user has a unique ID
         self.username = username
-        self.password = password
+        self.set_password(password)
         self.email = email
 
-    def __init__(self, *args, **kwargs):
-        """initializes user"""
-        super().__init__(*args, **kwargs)
+    def set_password(self, password):
+        """sets password"""
+        self.password_hash = generate_password_hash(password)
+    
+    def check_password(self, password):
+        """checks password"""
+        return check_password_hash(self.password_hash, password)
+
+@login.user_loader
+def load_user(id):
+    from models import db_storage
+    return db_storage.all(User).get(id)
--- web_flask/routes.py
+++ web_flask/routes.py
@@ -1,11 +1,19 @@
 # app/routes.py
 from flask import render_template, redirect, url_for, flash
 from models.register import AddUser
+from models.login import LoginForm
 from models import db_storage
+from sqlalchemy.orm import Session
 from models.user import User
+from flask_login import current_user, login_user, logout_user
 from web_flask import app
 
 app.config['SECRET_KEY'] = 'you-will-never-guess'
+
+
+@app.route('/home', methods=['GET', 'POST'])
+def home():
+    return render_template('home.html')
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
@@ -21,9 +29,35 @@ def register():
         db_storage.save()
 
         flash('Registration successful! You can now log in.', 'success')
-        return redirect(url_for('register'))
+        return redirect(url_for('login'))
+    return render_template('register.html', form=form)
 
-    return render_template('index.html', form=form)
+@app.route('/' , methods=['GET', 'POST'])
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if current_user.is_authenticated:
+        flash('Already logged in.')
+        return redirect(url_for('home'))
+
+    form = LoginForm()
+    if form.validate_on_submit():
+        users = db_storage.all(User)
+        for user in users.values():
+            if user.username == form.username.data and user.check_password(form.password.data):
+                login_user(user, remember=form.remember_me.data)
+                flash('Login successful!', 'success')
+                return redirect(url_for('home'))
+
+        flash('Invalid username or password', 'error')
+        return redirect(url_for('login'))
+
+    return render_template('login.html', form=form)
+@app.route('/logout', methods=['GET', 'POST'])
+def logout():
+    logout_user()
+    redirect(url_for('login'))
+
+
 
 if __name__ == '__main__':
     app.run(host='localhost', port=5000, debug=True)
